dfc52567fe5435d4d6cceef8d2cbac8db05ce00d
guiltychong <132539074+guiltychong@users.noreply.github.com>
2023-05-04 21:16:25 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cgwcexl2xsrz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chongzt-employee"
+customregion = "us-east-1"
+
